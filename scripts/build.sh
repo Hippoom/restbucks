d9a42e26954168992de960e10740a12ac6708220
hippoom <hippoom@gmail.com>
2016-03-10 12:20:58 +0800

    Fix path error when there is space in script dir

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,8 +5,8 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker run --rm \
            -t \
-           -v $user_home/.gradle:/root/.gradle \
-           -v $project_home:/project \
+           -v "$user_home"/.gradle:/root/.gradle \
+           -v "$project_home":/project \
            -w /project \
            java:8 \
            ./gradlew clean build
