038b950ffa4955e6713259d0c9bdd2f09ecf7809
hippoom <hippoom@gmail.com>
2016-03-30 23:01:34 +0800

    Fix space path issue in build image scripts

--- scripts/build-test-image.sh
+++ scripts/build-test-image.sh
@@ -3,6 +3,6 @@
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . "$script_dir"/common.sh #use quote here to compliant with space in dir
 
-version=$(eval cat "$project_home"/build/version)
+version=$(cat "$project_home"/build/version)
 
 docker build -t "hippoom/restbucks-ordering-test:$version" -f "$project_home"/Dockerfile.test "$project_home"
